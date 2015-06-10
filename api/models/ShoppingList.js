151c09f812fe57a5f42c223ece775b509a792d12
lSoleyl <lSoleyl+github@gmail.com>
2015-06-10 15:00:40 +0200

    Added new Models

---
+++ api/models/ShoppingList.js
@@ -0,0 +1,22 @@
+/**
+* ShoppingList.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    
+    date : 'date', 
+
+    entries : {
+      collection: 'shoppinglistentry',
+      via: 'list'
+    },
+
+    paidBy: { model:'person' }
+  }
+};
+
---
+++ api/models/ShoppingListEntry.js
@@ -0,0 +1,24 @@
+/**
+* ShoppingListEntry.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    list : {
+      model:'shoppinglist',
+      via:'entries'
+    },
+
+    product : {
+      model: 'productentry',
+      via: 'shop'
+    },
+
+    amount: 'integer'
+  }
+};
+
