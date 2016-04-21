0040b37850c1927fc821cc2f1d881ba2c99ed521
lSoleyl <lSoleyl+github@gmail.com>
2016-04-21 13:55:48 +0200

    Leftover of purchase create

--- api/controllers/PurchaseController.js
+++ api/controllers/PurchaseController.js
@@ -8,7 +8,20 @@
 module.exports = {
 	index: function(req, res) { //List all purchases
     res.view("purchase/list") //Just render the view
+  },
+
+  //Used to display creation form inside a BootstrapDialog
+  partial_create: function(req, res) {
+    res.view("purchase/partials/create", { //render the partial
+      layout:"" //Don't use layout(scripts etc.), only return partial's HTML
+    })
+  },
+
+  create: function(req, res) { //Overwrite default route, which would create a Purchase object
+    res.view() //Just render the view (with layout)
   }
+
+
 
 };
 
